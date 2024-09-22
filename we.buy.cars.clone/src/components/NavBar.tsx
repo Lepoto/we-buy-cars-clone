968ec5bbe6eafbaf9bb1b9d2ecf922f2381ddfe2
Alfred Masekwameng <percymasekwameng876@gmail.com>
2024-09-22 23:53:50 +0200

    chore:data.ts to hold links, data, any variables to make the code more maintainable

--- we.buy.cars.clone/src/components/NavBar.tsx
+++ we.buy.cars.clone/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { links } from '@/data/data';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -10,19 +11,15 @@ const NavBar = () => {
             <Image src='/webuycars.png' alt='' width={75} height={75} />
 
             <div className='py-6'>
-                <ul className='flex justify-between space-x-16 text-sm font-semibold'>
-                    <li className='hover:text-primary'>Sell my car</li>
-                    <li>
-                      <Link href="/buy-a-car" className='hover:text-primary hover:border-b-4 hover:border-primary py-7 px-3'>Buy a car</Link>
+                <ul className='flex justify-between space-x-12 text-sm font-semibold'>
+                   {links.map((link) => (
+                    <li key={link.name} >
+                      <Link href={link.link} className={`hover:text-primary hover:border-b-4 hover:border-primary py-7 px-2 ${link.Highlight}`}>{link.name}</Link>
                     </li>
-                    <li className='hover:text-primary'>Finance & Services</li>
-                    <li className='hover:text-primary'>Our Locations</li>
-                    <li className='hover:text-primary'>About</li>
-                    <li className='hover:text-primary'>Contact Us</li>
-                    <li className='hover:text-primary'>Investors</li>
-                    <li className='text-primary'>Sign Up</li>
+                   ))}
+                    {/* <li className='hover:border-b-4 hover:border-primary  text-primary '>Sign Up</li> */}
                     <div className='border-l-2 border-gray-300'></div>
-                    <li>Login</li>
+                    <li className=' hover:border-b-4 hover:border-primary px-3'>Login</li>
                 </ul>
             </div>
         </div>
---
+++ we.buy.cars.clone/src/data/data.ts
@@ -0,0 +1,42 @@
+export const links = [
+  {
+    name: "Sell my car",
+    link: "",
+    Highlight: "",
+  },
+  {
+    name: "Buy a car",
+    link: "/buy-a-car",
+    Highlight: "",
+  },
+  {
+    name: "Finance & Services",
+    link: "",
+    Highlight: "",
+  },
+  {
+    name: "Our Locations",
+    link: "",
+    Highlight: "",
+  },
+  {
+    name: "About",
+    link: "",
+    Highlight: "",
+  },
+  {
+    name: "Contact Us",
+    link: "",
+    Highlight: "",
+  },
+  {
+    name: "Investors",
+    link: "",
+    Highlight: "",
+  },
+  {
+    name: "Sign Up",
+    link: "",
+    Highlight: "text-primary",
+  },
+] as const;
